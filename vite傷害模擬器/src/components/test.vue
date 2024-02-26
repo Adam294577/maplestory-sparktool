d4c3940ea50534541dea2682bf4f4e08e5760246
Satoru <adam29457756@gmail.com>
2024-02-26 19:59:36 +0800

    星火模擬器 init

--- vite傷害模擬器/src/components/test.vue
+++
@@ -1,6 +0,0 @@
-<script setup></script>
-<template>
-  <div class="text-white text-xl">test</div>
-</template>
-
-<style lang="scss" scoped></style>
--- vite傷害模擬器/src/pages/index.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-
-<template>
-  <main>
-    <test />
-  </main>
-</template>
